ce35650a6a33705c5ba5da91bf3f1698c23df41e
Carter Harrison <carterjharrison@gmail.com>
2019-08-22 16:35:59 -0700

    use --unsafe-perm

--- src/package-manager/index.ts
+++ src/package-manager/index.ts
@@ -43,7 +43,10 @@ export class XyoPackageManager extends XyoBase {
 
     for (const toRun of pluginsToRun) {
       const resolver = this.pluginTypeInstallers[toRun.type]
-      plugins = plugins.concat(await resolver(toRun.config))
+
+      if (resolver) {
+        plugins = plugins.concat(await resolver(toRun.config))
+      }
     }
 
     for (const plugin of plugins) {
@@ -63,6 +66,11 @@ export class XyoPackageManager extends XyoBase {
 
   private async installPlugin(config: IXyoPluginConfig) {
     const resolver = this.pluginTypes[config.type]
+
+    if (!resolver) {
+      return undefined
+    }
+
     await resolver(config.config)
   }
 
@@ -100,7 +108,7 @@ interface IPathConfig {
 }
 
 const npmRemotePluginResolver = async(config: any) => {
-  execSync(`npm install ${config.packageName}@${config.version} -g`).toString('utf8')
+  execSync(`npm install --unsafe-perm -g ${config.packageName}@${config.version}`).toString('utf8')
   installNpmRepository(`${globalModules}/${config.packageName}`, config.subPlugins)
 }
 
